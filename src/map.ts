cb7447f11087c212c7efbf63b215db883f12fb99
Ben Hall <benji2357@gmail.com>
2017-10-08 19:22:39 +0100

    feat(map): Add more detailed type info

--- src/map.ts
+++ src/map.ts
@@ -1,6 +1,8 @@
 import curry2 from './internal/curry2'
-export default curry2((f: (x: any) => any, xs: any[]): any[] => {
-  const ys = []
-  for (let i = 0; i < xs.length; i++) ys[i] = f(xs[i])
-  return ys
-})
+export default curry2(
+  <A, B>(f: (x: A) => B, xs: A[]): B[] => {
+    const ys = []
+    for (let i = 0; i < xs.length; i++) ys[i] = f(xs[i])
+    return ys
+  }
+)
